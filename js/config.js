3429f4800b0879f75b12dbf6a8bc9c1b37500263
romilk-senpai <rpakhomov1702@gmail.com>
2026-04-30 21:41:04 +0300

    changed creds back ;]

--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyCCACnq23Ozr0KGUW2MNAti2rltAoBR3EA",
-	databaseURL: "https://zzz-shad1w-default-rtdb.firebaseio.com",
-	projectId: "zzz-shad1w"
-	/* apiKey: "AIzaSyAlcnUiLJ1cq7ekCQFi_NOPAQ6UiG92ZqM",
+	apiKey: "AIzaSyAlcnUiLJ1cq7ekCQFi_NOPAQ6UiG92ZqM",
 	databaseURL: "https://zzz-league-default-rtdb.firebaseio.com",
-	projectId: "zzz-league" */
+	projectId: "zzz-league"
 };
 
 firebase.initializeApp(firebaseConfig);
