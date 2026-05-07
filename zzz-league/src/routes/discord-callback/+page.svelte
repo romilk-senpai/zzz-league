57ec8304ee944714d1b7cdc6fa9cac569caa0a44
romilk-senpai <rpakhomov1702@gmail.com>
2026-05-07 13:38:34 +0300

    error handling kinda

--- zzz-league/src/routes/discord-callback/+page.svelte
+++ zzz-league/src/routes/discord-callback/+page.svelte
@@ -3,13 +3,29 @@
 	import { goto } from "$app/navigation";
 	import { handleDiscordCallback } from "$lib/discord";
 
+	let error = $state(false);
+	let errorText = $state("");
+
 	onMount(() => {
 		async function init() {
-			await handleDiscordCallback();
-			goto("/zzz-league");
+			try {
+				await handleDiscordCallback();
+				goto("/zzz-league");
+			} catch (e: any) {
+				error = true;
+				errorText = e.message;
+				setTimeout(function () {
+					goto("/zzz-league");
+				}, 2000);
+			}
 		}
 		init();
 	});
 </script>
 
-<p>Подключение Discord...</p>
+{#if error}
+	<p class="error">Ошибка {errorText}</p>
+	<p>Возврат на главую страницу...</p>
+{:else}
+	<p>Подключение Discord...</p>
+{/if}
